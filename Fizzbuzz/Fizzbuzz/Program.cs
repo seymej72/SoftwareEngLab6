be6574a95893516d19f863c594c831aee5b40df6
seymej72 <seymej72@uwosh.edu>
2018-10-24 14:45:50 -0500

    Code for Loop

--- Fizzbuzz/Fizzbuzz/Program.cs
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -10,7 +10,26 @@ namespace Fizzbuzz
     {
         static void Main(string[] args)
         {
-            //test
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i%5==0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
